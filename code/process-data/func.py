09b53fc9bf21e037fd07efdfaf6ffac1227b2b6b
Rekha Mathew <rekhamathew@dhcp-10-191-198-252.vpn.oracle.com>
2022-03-23 17:16:34 +0530

    modified

--- code/process-data/func.py
+++ code/process-data/func.py
@@ -10,90 +10,101 @@ import requests
 import base64
 import logging
 from fdk import response
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse
 
 
 def get_dbwallet_from_autonomousdb():
 	try:
-		signer = oci.auth.signers.get_resource_principals_signer()  # authentication based on instance principal
-		atp_client = oci.database.DatabaseClient(config={}, signer=signer)
-		atp_wallet_pwd = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))  # random string
-		# the wallet password is  for creation of the  jks files,  so the value is not important
-		atp_wallet_details = oci.database.models.GenerateAutonomousDatabaseWalletDetails(password=atp_wallet_pwd)
+		dbwalletzip_location = "/tmp/dbwallet.zip"
 
-		obj = atp_client.generate_autonomous_database_wallet(adb_ocid, atp_wallet_details)
+		ajd_client = oci.database.DatabaseClient(config={}, signer=signer)
+		ajd_wallet_pwd = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))  # random string
+		# the wallet password is for creation of the jks
+		ajd_wallet_details = oci.database.models.GenerateAutonomousDatabaseWalletDetails(password=ajd_wallet_pwd)
+		# Get the AJD wallet zip file
+		obj = ajd_client.generate_autonomous_database_wallet(ajd_ocid, ajd_wallet_details)
+		# write the DB wallet zip to dbwalletzip_location
 		with open(dbwalletzip_location, 'w+b') as f:
 			for chunk in obj.data.raw.stream(1024 * 1024, decode_content=False):
 				f.write(chunk)
-		with ZipFile(dbwalletzip_location, 'r') as zipObj:
-			zipObj.extractall(dbwallet_dir)
-	except Exception as e:
-		logging.getLogger().error(e)
+		# extract the zip to dbwallet_dir
+		with ZipFile(dbwalletzip_location, 'r') as wallet_zip:
+			wallet_zip.extractall(dbwallet_dir)
+	except Exception as ex:
+		logging.getLogger().error("Failed to retrieve DB Wallet due to exception.", ex)
 		raise
 
 
+# This method is to get the secret content stored in vault using the secret name
 def get_secret_from_vault(vault_secret_name):
 	try:
-		signer = oci.auth.signers.get_resource_principals_signer()
-
+		# get the secret client
 		client = oci.secrets.SecretsClient({}, signer=signer)
 
+		# Read the secret content
 		secret_content = client.get_secret_bundle_by_name(secret_name=vault_secret_name,
 														  vault_id=vault_ocid).data.secret_bundle_content.content
 		decrypted_secret_content = base64.b64decode(secret_content).decode("utf-8")
 		return decrypted_secret_content
 
-	except Exception as e:
-		logging.getLogger().error(e)
+	except Exception as ex:
+		logging.getLogger().error(ex)
 		raise
 
 
 #
-# Instantiation code: executed once when the function container is initialized
+# Function Instantiation code
 #
 try:
-	if os.getenv("vault_ocid") is not None:
-		vault_ocid = os.getenv("vault_ocid")
-	else:
-		raise ValueError("ERROR: Missing configuration key vault_ocid")
-	if os.getenv("vault_key_ocid") is not None:
-		vault_key_ocid = os.getenv("vault_key_ocid")
-	else:
-		raise ValueError("ERROR: Missing configuration key vault_key_ocid")
 
-	if os.getenv("vault_compartment_ocid") is not None:
-		vault_compartment_ocid = os.getenv("vault_compartment_ocid")
+	signer = oci.auth.signers.get_resource_principals_signer()
+	if os.getenv("TNS_ADMIN") is not None:
+		dbwallet_dir = os.getenv('TNS_ADMIN')
 	else:
-		raise ValueError("ERROR: Missing configuration key vault_compartment_ocid")
+		raise ValueError("ERROR: TNS_ADMIN entry missing in Docker File")
 
-	if os.getenv("DBUSER") is not None:
-		dbuser = os.getenv("DBUSER")
+	if os.getenv("VAULT_OCID") is not None:
+		vault_ocid = os.getenv("VAULT_OCID")
 	else:
-		raise ValueError("ERROR: Missing configuration key DBUSER")
+		raise ValueError("ERROR: Missing configuration key VAULT_OCID")
+	if os.getenv("VAULT_KEY_OCID") is not None:
+		vault_key_ocid = os.getenv("VAULT_KEY_OCID")
+	else:
+		raise ValueError("ERROR: Missing configuration key VAULT_KEY_OCID")
 
-	if os.getenv("DBSVC") is not None:
-		dbsvc = os.getenv("DBSVC")
+	if os.getenv("VAULT_COMPARTMENT_OCID") is not None:
+		vault_compartment_ocid = os.getenv("VAULT_COMPARTMENT_OCID")
 	else:
-		raise ValueError("ERROR: Missing configuration key DBSVC")
-	if os.getenv("ADB_OCID") is not None:
-		adb_ocid = os.getenv("ADB_OCID")
-	else:
-		raise ValueError("ERROR: Missing configuration key ADB_OCID")
+		raise ValueError("ERROR: Missing configuration key VAULT_COMPARTMENT_OCID")
 
-	# Download the DB Wallet
-	dbwalletzip_location = "/tmp/dbwallet.zip"
-	dbwallet_dir = os.getenv('TNS_ADMIN')
+	if os.getenv("AJD_SCHEMA_NAME") is not None:
+		dbuser = os.getenv("AJD_SCHEMA_NAME")
+	else:
+		raise ValueError("ERROR: Missing configuration key AJD_SCHEMA_NAME")
+
+	if os.getenv("AJD_SERVICE_NAME") is not None:
+		dbsvc = os.getenv("AJD_SERVICE_NAME")
+	else:
+		raise ValueError("ERROR: Missing configuration key AJD_SERVICE_NAME")
+	if os.getenv("AJD_OCID") is not None:
+		ajd_ocid = os.getenv("AJD_OCID")
+	else:
+		raise ValueError("ERROR: Missing configuration key AJD_OCID")
 
 	get_dbwallet_from_autonomousdb()
 
-	# Update SQLNET.ORA
+	# Update SQLNET.ORA file present in the dbwallet_dir by replacing
+	# the WALLET_LOCATION parameter to point to the path of dbwallet_dir
 	with open(dbwallet_dir + '/sqlnet.ora') as orig_sqlnetora:
 		new_text = orig_sqlnetora.read().replace('DIRECTORY=\"?/network/admin\"',
 												 'DIRECTORY=\"{}\"'.format(dbwallet_dir))
 	with open(dbwallet_dir + '/sqlnet.ora', "w") as new_sqlnetora:
 		new_sqlnetora.write(new_text)
+
+	# Get database password from vault
 	dbpwd = get_secret_from_vault('db_pwd')
-	logging.getLogger().info('dbpwd =', dbpwd)
+	# create a DB pool
+
 	dbpool = cx_Oracle.SessionPool(dbuser, dbpwd, dbsvc, min=1, max=1, encoding="UTF-8", nencoding="UTF-8")
 
 except Exception as e:
@@ -102,32 +113,35 @@ except Exception as e:
 
 
 #
-# Function Handler: executed every time the function is invoked
+# Function Handler
 #
 def handler(ctx, data: io.BytesIO = None):
 	try:
 
 		requesturl = ctx.RequestURL()
 		parsed_url = urlparse(requesturl)
+		# Get the path of the request URL to identify if the call is for retry
 		path_param = parsed_url.path
-		logging.getLogger().info("path_param", path_param)
+
 		payload_bytes = data.getvalue()
 		payload = json.loads(payload_bytes)
 
 		if payload_bytes == b'':
 			raise KeyError('No keys in payload')
+		# Check if no_of_records_to_process is >0
 		no_of_records_to_process = int(payload["no_of_records_to_process"])
 		if no_of_records_to_process <= 0:
 			raise ValueError('no_of_records_to_process must be greater than 0')
+		# If the Function call is for retry, get additional keys from payload
 		if path_param == '/jsondb/process/retry':
-			no_of_times_to_retry = int(payload["no_of_times_to_retry"])
+			retry_limit = int(payload["retry_limit"])
 			retry_codes = payload["retry_codes"]
 
-	except Exception as e:
-		logging.getLogger().error(e)
+	except Exception as ex1:
+		logging.getLogger().error(ex1)
 
 		if path_param == '/jsondb/process/retry':
-			message = "Missing keys- Check if no_of_records_to_process, retry_codes,no_of_times_to_retry are set correctly"
+			message = "Missing keys- Check if no_of_records_to_process, retry_codes,retry_limit are set correctly"
 		else:
 			message = "Missing keys- Check if no_of_records_to_process is set correctly"
 		return response.Response(
@@ -144,111 +158,124 @@ def handler(ctx, data: io.BytesIO = None):
 			collection = soda.openCollection("datasync_collection")
 			# Check if it is a retry call
 			if path_param == '/jsondb/process/retry':
-				logging.getLogger().info("inside retry**")
-
+				# Filter the records with status as failed and with status_code matching the retry codes specified in payload
 				qbe = {'$query': {'status': {'$eq': 'failed'}, 'status_code': {'$in': retry_codes.split(',')}},
 					   '$orderby': [
-						   {'path': 'createdDate', 'datatype': 'datetime', 'order': 'asc'}]}
+						   {'path': 'createdDate', 'datatype': 'timestamp'}]}
 			else:
+				# Filter the records with status as not_processed
 				qbe = {'$query': {'status': {'$eq': 'not_processed'}},
 					   '$orderby': [
-						   {'path': 'createdDate', 'datatype': 'datetime', 'order': 'asc'}]}
+						   {'path': 'createdDate', 'datatype': 'timestamp'}]}
 
 			total_count = 0
 			sucess_count = 0
 			failed_count = 0
-			retrial_count_reached = 0
-
+			skipped_count = 0
+			# Total number of records returned by filter
 			num_docs = collection.find().filter(qbe).count()
-			logging.getLogger().info('num_docs', num_docs)
+			# Loop through the filtered records
 			for doc in collection.find().filter(qbe).limit(no_of_records_to_process).getDocuments():
-				content = doc.getContent()
-				is_processed = True
+				try:
+					content = doc.getContent()
+					is_processed = True
 
-				if path_param == '/jsondb/process/retry':
-					# Check how many times retrial has happened
-					if "retrial_count" in content:
-						if int(content["retrial_count"]) < no_of_times_to_retry:
-							content["retrial_count"] = int(content["retrial_count"]) + 1
-							process(content)
+					if path_param == '/jsondb/process/retry':
+						# Check how many times retrial has happened for the selected record
+						if "retry_attempts" in content:
+							# If the retrial count of the record is less than specified limit, continue processing
+							if int(content["retry_attempts"]) < retry_limit:
+								content["retry_attempts"] = int(content["retry_attempts"]) + 1
+								process(content)
+							else:
+								logging.getLogger().info('no of trials exceeded')
+								is_processed = False
+								# Count the no of records of skipped for processing  since record has reached the retry limit.
+								skipped_count = skipped_count + 1
+						# set the key retry_attempts in JSON to 1, if this is first retry attempt
 						else:
-							logging.getLogger().info('no of trials exceeded')
-							is_processed = False
-							retrial_count_reached = retrial_count_reached + 1
+							content["retry_attempts"] = 1
+							process(content)
 					else:
-						content["retrial_count"] = 1
 						process(content)
-				else:
-					process(content)
 
-				# Replace the content with new content having status_code and status
-				if is_processed == True:
-					collection.find().key(doc.key).replaceOne(content)
-					if content['status'] == "success":
-						sucess_count = sucess_count + 1
-					elif content['status'] == "failed":
-						failed_count = failed_count + 1
-				total_count = total_count + 1
-				if num_docs <= no_of_records_to_process:
-					has_next = "false"
-				else:
-					has_next = "true"
-	except Exception as e:
-		logging.getLogger().error(e)
+					# Replace the content with new content which has status_code and status
+					if is_processed:
+						collection.find().key(doc.key).replaceOne(content)
+						if content['status'] == "success":
+							sucess_count = sucess_count + 1
+						elif content['status'] == "failed":
+							failed_count = failed_count + 1
+					total_count = total_count + 1
+				except Exception as ex3:
+					logging.getLogger().error(
+						"Exception occured during the processing of " + str(doc.key) + "due to " + str(ex3))
+
+			# Check if there are more records to process
+			if num_docs <= no_of_records_to_process:
+				has_next = "false"
+			else:
+				has_next = "true"
+
+	except Exception as ex2:
+		logging.getLogger().error(ex2)
 
 		return response.Response(
 			ctx,
-			response_data="Failed in completing the request due to " + str(e),
+			response_data="Failed in completing the request due to " + str(ex2),
 			status_code=500
 		)
-
+	# Return the result of processing
 	return response.Response(
 		ctx,
 		response_data='{"total_processed_records":' + str(total_count) + ',"success_count":' + str(
 			sucess_count) + ',"failure_count":' + str(failed_count) + ',"skipped_count":' + str(
-			retrial_count_reached) + ',"has_next:"' + has_next + '"}'
+			skipped_count) + ',"has_next:"' + has_next + '"}'
 	)
 
 
+# This method is used to process the document in the collection
 def process(content):
-	logging.getLogger().info("inside process call")
+	# extract the json payload key values
 	try:
 		vault_secret_name = content["vaultSecretName"]
 		target_rest_api = content["targetRestApi"]
 		target_rest_api_operation = content["targetRestApiOperation"]
 		target_rest_api_payload = content["targetRestApiPayload"]
 
-	except Exception as e:
-		logging.getLogger().error(e)
+	except Exception as ex3:
+		logging.getLogger().error(ex3)
 		content['status'] = 'failed'
 		content['status_code'] = 500
 		content[
-			'failure_reason'] = 'Check if the payload contains vaultSecretName,targetRestApi,targetRestApiOperation,targetRestApiPayload'
+			'failure_reason'] = 'Check if the payload contains vaultSecretName,targetRestApi,targetRestApiOperation,' \
+								'targetRestApiPayload '
 		return
 	try:
+		# Get the security token for API call rom vault
 		auth_token = get_secret_from_vault(vault_secret_name)
 		if "targetRestApiHeaders" in content:
 			target_rest_api_headers = content["targetRestApiHeaders"]
-		# Merge two headers
+		# Merge the authorization header with any other headers already present
 
 		target_rest_api_headers.update({'Authorization': auth_token})
 
 		if target_rest_api_operation == 'POST':
-			logging.getLogger().info("inside POST call")
 
 			api_call_response = requests.post(target_rest_api, data=json.dumps(target_rest_api_payload),
 											  headers=target_rest_api_headers)
 		elif target_rest_api_operation == 'PUT':
-			logging.getLogger().info("inside PUT call")
+
 			api_call_response = requests.put(target_rest_api, data=json.dumps(target_rest_api_payload),
 											 headers=target_rest_api_headers)
 		elif target_rest_api_operation == 'DELETE':
-			logging.getLogger().info("inside DELETE call")
+
 			api_call_response = requests.delete(target_rest_api, data=json.dumps(target_rest_api_payload),
 												headers=target_rest_api_headers)
 		else:
 			logging.getLogger().info("incorrect REST API method. Method should be either POST, PUT or DELETE")
 			content['status'] = 'failed'
+			content['status_code'] = 500
 			content['failure_reason'] = "incorrect REST API method. Method should be either POST, PUT or DELETE"
 			return
 
@@ -261,8 +288,8 @@ def process(content):
 
 		content['status_code'] = api_call_response.status_code
 
-	except Exception as e:
-		logging.getLogger().error(e)
+	except Exception as ex4:
+		logging.getLogger().error(ex4)
 		content['status'] = 'failed'
 		content['status_code'] = 500
-		content['failure_reason'] = 'unexpected error {0}', e
+		content['failure_reason'] = 'unexpected error. ' + str(ex4)
--- code/store-data/func.py
+++ code/store-data/func.py
@@ -6,9 +6,9 @@ import os
 from zipfile import ZipFile
 import string
 import random
+import datetime
 
 import base64
-from timeit import default_timer as timer
 
 import logging
 from fdk import response
@@ -16,153 +16,166 @@ from fdk import response
 
 def get_dbwallet_from_autonomousdb():
 	try:
-		signer = oci.auth.signers.get_resource_principals_signer()  # authentication based on instance principal
-		atp_client = oci.database.DatabaseClient(config={}, signer=signer)
-		atp_wallet_pwd = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))  # random string
-		# the wallet password is for creation of the jks
-		atp_wallet_details = oci.database.models.GenerateAutonomousDatabaseWalletDetails(password=atp_wallet_pwd)
+		dbwalletzip_location = "/tmp/dbwallet.zip"
 
-		obj = atp_client.generate_autonomous_database_wallet(adb_ocid, atp_wallet_details)
+		ajd_client = oci.database.DatabaseClient(config={}, signer=signer)
+		ajd_wallet_pwd = ''.join(random.choices(string.ascii_uppercase + string.digits, k=15))  # random string
+		# the wallet password is for creation of the jks
+		ajd_wallet_details = oci.database.models.GenerateAutonomousDatabaseWalletDetails(password=ajd_wallet_pwd)
+		# Get the AJD wallet zip file
+		obj = ajd_client.generate_autonomous_database_wallet(ajd_ocid, ajd_wallet_details)
+		# write the DB wallet zip to dbwalletzip_location
 		with open(dbwalletzip_location, 'w+b') as f:
 			for chunk in obj.data.raw.stream(1024 * 1024, decode_content=False):
 				f.write(chunk)
-		with ZipFile(dbwalletzip_location, 'r') as zipObj:
-			zipObj.extractall(dbwallet_dir)
+		# extract the zip to dbwallet_dir
+		with ZipFile(dbwalletzip_location, 'r') as wallet_zip:
+			wallet_zip.extractall(dbwallet_dir)
 	except Exception as ex:
-		logging.getLogger().error("Failed to retrieve the secret content", ex)
+		logging.getLogger().error("Failed to retrieve DB Wallet due to exception.", ex)
 		raise
 
 
+# This method is to get the secret content stored in vault using the secret name
 def get_secret_from_vault(vault_secret_name):
-	signer = oci.auth.signers.get_resource_principals_signer()
 	try:
+		# get the secret client
 		client = oci.secrets.SecretsClient({}, signer=signer)
 
+		# Read the secret content
 		secret_content = client.get_secret_bundle_by_name(secret_name=vault_secret_name,
 														  vault_id=vault_ocid).data.secret_bundle_content.content
 		decrypted_secret_content = base64.b64decode(secret_content).decode("utf-8")
+		return decrypted_secret_content
 
 	except Exception as ex:
-		logging.getLogger().error("Failed to retrieve the secret content", ex)
+		logging.getLogger().error(ex)
 		raise
-	return decrypted_secret_content
 
 
-# Instantiation code: executed once when the function container is initialized
-if os.getenv("vault_ocid") is not None:
-	vault_ocid = os.getenv("vault_ocid")
-else:
-	raise ValueError("ERROR: Missing configuration key vault_ocid")
-if os.getenv("vault_key_ocid") is not None:
-	vault_key_ocid = os.getenv("vault_key_ocid")
-else:
-	raise ValueError("ERROR: Missing configuration key vault_key_ocid")
+#
+# Function Instantiation code
+#
+try:
 
-if os.getenv("vault_compartment_ocid") is not None:
-	vault_compartment_ocid = os.getenv("vault_compartment_ocid")
-else:
-	raise ValueError("ERROR: Missing configuration key vault_compartment_ocid")
+	signer = oci.auth.signers.get_resource_principals_signer()
+	if os.getenv("TNS_ADMIN") is not None:
+		dbwallet_dir = os.getenv('TNS_ADMIN')
+	else:
+		raise ValueError("ERROR: TNS_ADMIN entry missing in Docker File")
 
-if os.getenv("DBUSER") is not None:
-	dbuser = os.getenv("DBUSER")
-else:
-	raise ValueError("ERROR: Missing configuration key DBUSER")
+	if os.getenv("VAULT_OCID") is not None:
+		vault_ocid = os.getenv("VAULT_OCID")
+	else:
+		raise ValueError("ERROR: Missing configuration key VAULT_OCID")
+	if os.getenv("VAULT_KEY_OCID") is not None:
+		vault_key_ocid = os.getenv("VAULT_KEY_OCID")
+	else:
+		raise ValueError("ERROR: Missing configuration key VAULT_KEY_OCID")
 
-if os.getenv("DBSVC") is not None:
-	dbsvc = os.getenv("DBSVC")
-else:
-	raise ValueError("ERROR: Missing configuration key DBSVC")
-if os.getenv("ADB_OCID") is not None:
-	adb_ocid = os.getenv("ADB_OCID")
-else:
-	raise ValueError("ERROR: Missing configuration key ADB_OCID")
+	if os.getenv("VAULT_COMPARTMENT_OCID") is not None:
+		vault_compartment_ocid = os.getenv("VAULT_COMPARTMENT_OCID")
+	else:
+		raise ValueError("ERROR: Missing configuration key VAULT_COMPARTMENT_OCID")
 
-# Download the DB Wallet
-dbwalletzip_location = "/tmp/dbwallet.zip"
-dbwallet_dir = os.getenv('TNS_ADMIN')
+	if os.getenv("AJD_SCHEMA_NAME") is not None:
+		dbuser = os.getenv("AJD_SCHEMA_NAME")
+	else:
+		raise ValueError("ERROR: Missing configuration key AJD_SCHEMA_NAME")
 
-get_dbwallet_from_autonomousdb()
+	if os.getenv("AJD_SERVICE_NAME") is not None:
+		dbsvc = os.getenv("AJD_SERVICE_NAME")
+	else:
+		raise ValueError("ERROR: Missing configuration key AJD_SERVICE_NAME")
+	if os.getenv("AJD_OCID") is not None:
+		ajd_ocid = os.getenv("AJD_OCID")
+	else:
+		raise ValueError("ERROR: Missing configuration key AJD_OCID")
 
-# Update SQLNET.ORA
-with open(dbwallet_dir + '/sqlnet.ora') as orig_sqlnetora:
-	new_text = orig_sqlnetora.read().replace('DIRECTORY=\"?/network/admin\"', 'DIRECTORY=\"{}\"'.format(dbwallet_dir))
-with open(dbwallet_dir + '/sqlnet.ora', "w") as new_sqlnetora:
-	new_sqlnetora.write(new_text)
-dbpwd = get_secret_from_vault('db_pwd')
-start1 = timer()
-dbpool = cx_Oracle.SessionPool(dbuser, dbpwd, dbsvc, min=1, max=1, encoding="UTF-8", nencoding="UTF-8")
-end1 = timer()
-print('INFO: inserted in {} sec'.format(end1 - start1), flush=True)
+	get_dbwallet_from_autonomousdb()
+
+	# Update SQLNET.ORA file present in the dbwallet_dir by replacing
+	# the WALLET_LOCATION parameter to point to the path of dbwallet_dir
+	with open(dbwallet_dir + '/sqlnet.ora') as orig_sqlnetora:
+		new_text = orig_sqlnetora.read().replace('DIRECTORY=\"?/network/admin\"',
+												 'DIRECTORY=\"{}\"'.format(dbwallet_dir))
+	with open(dbwallet_dir + '/sqlnet.ora', "w") as new_sqlnetora:
+		new_sqlnetora.write(new_text)
+
+	# Get database password from vault
+	dbpwd = get_secret_from_vault('db_pwd')
+	# create a DB pool
+
+	dbpool = cx_Oracle.SessionPool(dbuser, dbpwd, dbsvc, min=1, max=1, encoding="UTF-8", nencoding="UTF-8")
+
+except Exception as e:
+	logging.getLogger().error(e)
+	raise
 
 
 #
 # Function Handler
 #
 def handler(ctx, data: io.BytesIO = None):
+	logging.getLogger().info("Inside handler method")
 	try:
 		payload_bytes = data.getvalue()
 		if payload_bytes == b'':
 			raise KeyError('No keys in payload')
 
 		payload = json.loads(payload_bytes)
-		if "createdDate" not in payload:
-			raise KeyError('createdDate not found in payload')
+		# Update the payload with new key and values.
 		payload["status"] = "not_processed"
+		payload["createdDate"] = str(datetime.datetime.now().isoformat())
+		# Read the authorization token from the request header
 		auth_token = ctx.Headers().get("authorization")
+		if "vaultSecretName" in payload:
+			vault_secret_name = payload["vaultSecretName"]
+		else:
+			raise KeyError('Check if key vaultSecretName is set.')
+		# Check if a secret with name as vault_secret_name already present in Vault.
+		# If it is not present, create a new secret for storing authorization token.
 
-		vault_secret_name = payload["vaultSecretName"]
 		if not is_secret_in_vault(vault_secret_name):
 			create_secret_in_vault(auth_token, vault_secret_name)
-	except Exception as e:
-		logging.getLogger().error(e)
+	except Exception as ex:
+		logging.getLogger().error(ex)
 		return response.Response(
 			ctx,
-			response_data="Check if all keys including vaultSecretName, createdDate is set",
+			response_data="Processing failed due to " + str(ex),
 			status_code=500
 		)
 	try:
 		with dbpool.acquire() as dbconnection:
 			dbconnection.autocommit = True
 
-			# create a new SODA collection; this will open an existing collection, if
-			# the name is already in use
-			start = timer()
 			soda = dbconnection.getSodaDatabase()
-
+			# Open the Collection datasync_collection, if its already present in database
+			# Else create a new collection
 			collection = soda.openCollection("datasync_collection")
 			if collection is None:
 				collection = soda.createCollection("datasync_collection")
-			# insert a document into the collection; for the common case of a JSON
-			# document
-			collection.insertOneAndGet(payload)
-			end = timer()
-	except Exception as e:
-		logging.getLogger().error(e)
+			# insert the payload to the collection
+			collection.insertOne(payload)
+
+	except Exception as ex1:
+		logging.getLogger().error(ex1)
 		return response.Response(
 			ctx,
-			response_data="Failed due to " + str(e),
+			response_data="Processing failed due to " + str(ex1),
 			status_code=500
 		)
 	return response.Response(
 		ctx,
-		response_data="success" + format(end - start) + ":***" + format(end1 - start1)
+		response_data="success"
 	)
-
-
-# except Exception as e:
-# 	logging.getLogger().error(e)
-# 	return response.Response(
-# 		ctx,
-# 		response_data="failed"
-# 	)
 
 
 # Create a new secret in vault
 def create_secret_in_vault(
 		auth_token,
 		vault_secret_name):
-	logging.getLogger().info("INSIDE  create_secret_in_vault")
-	signer = oci.auth.signers.get_resource_principals_signer()
+	logging.getLogger().info("Inside create_secret_in_vault method")
 	try:
 		vault_client = oci.vault.VaultsClient({}, signer=signer)
 
@@ -179,15 +192,13 @@ def create_secret_in_vault(
 
 		vault_client.create_secret(secrets_details)
 	except Exception as ex:
-		logging.getLogger().error("Failed to create the secret content", ex)
+		logging.getLogger().error("Failed to create the secret content due to exception. " + str(ex))
 		raise
 
 
 # Check if the secret is already present in vault
 def is_secret_in_vault(vault_secret_name):
-	logging.getLogger().info("INSIDE  is_secret_in_vault")
-
-	signer = oci.auth.signers.get_resource_principals_signer()
+	logging.getLogger().info("Inside is_secret_in_vault method")
 	try:
 		vault_client = oci.vault.VaultsClient({}, signer=signer)
 
@@ -202,6 +213,7 @@ def is_secret_in_vault(vault_secret_name):
 			logging.getLogger().info("Secret not found in vault")
 			return False
 	except Exception as ex:
-		logging.getLogger().error("Failed to retrieve the secret content", ex)
+		logging.getLogger().error(
+			"Failed to check if the secret is already present in Vault due to exception. " + str(ex))
 		raise
 	return True
